7e03cee0c8f03b8e5f919b54f9faf82b5594e004
yann en chemin <73861577+yannenchemin@users.noreply.github.com>
2020-12-07 23:10:27 +0900

    outside 3 code

--- sketch.js
+++ sketch.js
@@ -1,9 +1,19 @@
 function setup() {
   createCanvas(400, 400);
 
-  
+
 }
 
 function draw() {
   background(220);
 }
+
+
+//---------- 외부에서 받은 3개의 코드
+/*
+if (random(10)>9) {rect(10,10,10,10)};
+
+sleepy = createVector(0,100); applyForce(sleepy);
+
+ellipse(this.position.x, this.position.y, 12, 12);
+*/
