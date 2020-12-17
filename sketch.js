60bfb84d0e66e09d6a8627cda634bac01e57ac3e
yann en chemin <73861577+yannenchemin@users.noreply.github.com>
2020-12-17 19:17:38 +0900

    more 3 codes

--- sketch.js
+++ sketch.js
@@ -1,19 +1,28 @@
+let x,y;
+
 function setup() {
-  createCanvas(400, 400);
-
-
+  createCanvas(1000, 1000);
 }
 
 function draw() {
   background(220);
+
+ellipse(this.position.x, this.position.y, 12, 12);
+triangle(150,200, 200, 120, 250, 200)
 }
 
 
 //---------- 외부에서 받은 3개의 코드
 /*
-if (random(10)>9) {rect(10,10,10,10)};
+if (random(10)>9) {rect(10,10,10,10)}; //랜
 
-sleepy = createVector(0,100); applyForce(sleepy);
+sleepy = createVector(0,100); applyForce(sleepy); //클래스 만들
 
-ellipse(this.position.x, this.position.y, 12, 12);
+ellipse(this.position.x, this.position.y, 12, 12); // x,y를 사용한 원형
+
+slider = createSlider(1,100,1); slider.style(‘width’,’100px’); //슬라이더 만들기
+
+fill(v1, v2, v3, [alpha]); noStroke(); //색 채우기,선없음
+
+triangle(150,200, 200, 120, 250, 200); //삼각형 만들기
 */
