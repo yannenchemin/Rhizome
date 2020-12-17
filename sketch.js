7900cde6bf89140a724ac59cf644a243cf22db3c
yann en chemin <73861577+yannenchemin@users.noreply.github.com>
2020-12-17 22:46:57 +0900

    add class

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
-let x,y;
+let light_point = []; //Finger_Point 객체들의 배열
+let x = 100;
+let y = 100;
 let v1;
 let v2;
 let v3;
@@ -7,22 +9,47 @@ function setup() {
   createCanvas(1000, 1000);
   background(220);
   noStroke();
+
   v1 = createVector(mouseX,mouseY);
   v2 = createVector(mouseX,mouseY);
   v3 = createVector(mouseX,mouseY);
+
+for(let i = 0; i < 50; i++){
+  light_point.push(new LED());
+}
+
 }
 
 function draw() {
-
 fill(v1, v2, v3, [alpha]);
 
 if (random(10)>9) {
   rect(10,10,10,10);
   triangle(150,200, 200, 120, 250, 200);
+  }
+  for (let i = 0; i < light_point.length; i++){
+    light_point[i].move();
+    light_point[i].display();
+  }
 }
 
+class LED_light{
+constructor() {
+  this.x = mouseX;
+  this.y = mouseY;
+  this.light_speed = 1;
+  this.position = createVector(mouseX,mouseY);
+}
+move(){
+  this.x += random(-this.spped, this.speed);
+  this.y += random(-this.spped, this.speed);
+}
+
+display(){
+ellipse(this.position.x, this.position.y, 12, 12);
 
 }
+
 
 
 //---------- 외부에서 받은 3개의 코드
